8f2ebc03ed55f90ef4ce5368bf83cffd93b430db
Derek Lane <dglane001@hotmail.com>
2018-06-21 14:32:06 -0500

    tried using proxy generator, only to find that templates aren't being formatted properly

--- ImageWorkListAggregatorManager/WorklistAggregationManagerService.cs
+++ ImageWorkListAggregatorManager/WorklistAggregationManagerService.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 using ImagingTypes;
+using ProxyManager;
+
 using ImageWorkListAggregatorManager.Contracts;
 using EmzImagingInteractionManager.Contracts;
+
+
 
 namespace ImageWorkListAggregatorManager
 {
@@ -13,9 +18,17 @@ namespace ImageWorkListAggregatorManager
     {
         public async Task<List<WorklistItem>> GetWorklistForStaffAsync(Guid staffId)
         {
-            var client = new ImagingInteractionManagerClient();
-            var resultList = await client.GetWorklistForStaffAsync(staffId);
-            client.Close();
+#if USE_PROXY_GENERATOR
+            var proxy = ProxyClassGenerator.CreateProxy<IImagingInteractionManager>();
+#else
+            var proxy = new ImagingInteractionManagerClient();
+#endif
+            var resultList = await proxy.GetWorklistForStaffAsync(staffId);
+#if USE_PROXY_GENERATOR
+            ((ClientBase<IImagingInteractionManager>)proxy).Close();
+#else
+            proxy.Close();
+#endif
 
             // get the list of items from data access
             List<WorklistItem> dataAccessList = new List<WorklistItem>();
