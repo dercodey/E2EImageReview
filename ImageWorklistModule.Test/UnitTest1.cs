d111c7cb3714d9a9148f1cb54403d179bf9e31df
Lane, Derek <dg1an3@2nd-messenger.com>
2021-01-26 18:38:39 -0600

    added .net core wpf application adding unit test project

---
+++ ImageWorklistModule.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageWorklistModule.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
