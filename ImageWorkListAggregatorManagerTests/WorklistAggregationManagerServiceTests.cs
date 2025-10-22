823e14ba9ced4c5273996a091dee6ea5c5cc4020
dg1an3 <dg1an3@github.com>
2025-10-22 11:41:10 -0500

    Store final changes for step 'Upgrade project to .NET 8.0'

--- ImageWorkListAggregatorManagerTests/WorklistAggregationManagerServiceTests.cs
+++ ImageWorkListAggregatorManagerTests/WorklistAggregationManagerServiceTests.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
 using CoreWCF;
@@ -10,40 +8,22 @@ namespace ImageWorkListAggregatorManager.Tests
     [TestClass]
     public class WorklistAggregationManagerServiceTests
     {
-        private WebApplicationFactory<Program> _factory;
+        private WorklistAggregationManagerService _service;
 
         [TestInitialize]
         public void Initialize()
         {
-            _factory = new WebApplicationFactory<Program>()
-        .WithWebHostBuilder(builder =>
-         {
-          builder.ConfigureServices(services =>
-          {
- services.AddServiceModelServices();
-         services.AddServiceModelMetadata();
-       services.AddSingleton<WorklistAggregationManagerService>();
-   });
-     });
-        }
-
- [TestCleanup]
-        public void Cleanup()
-     {
-    _factory?.Dispose();
+            _service = new WorklistAggregationManagerService();
         }
 
         [TestMethod]
         public async Task GetWorklistForStaffAsyncTest()
         {
-            // Arrange
-            var service = _factory.Services.GetRequiredService<WorklistAggregationManagerService>();
-
             // Act
-            var result = await service.GetWorklistForStaffAsync(Guid.Empty);
+            var result = await _service.GetWorklistForStaffAsync(Guid.Empty);
 
             // Assert
             Assert.IsNotNull(result);
-     }
+        }
     }
 }
